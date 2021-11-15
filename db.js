66625aee216ac804d3746868d177f203f5f6263b
Dinh Hoang Nam <dinhhoangnam998@gmail.com>
2021-11-16 00:36:07 +0700

    :pencil2: add .env file

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const { MongoClient } = require("mongodb");
-
-// Connection URL
-const url = "mongodb://mongoadmin:secret@localhost:27017/TodoApp?authSource=admin&readPreference=primary&ssl=false";
+const url = process.env.MONGO_CONNECTION_STRING;
 const client = new MongoClient(url);
 
 module.exports = { connection: client.connect() };
